025017635957323e5b341ff52d525d9b49d18d2e
Edham Arief / edthix <edthix@gmail.com>
2015-03-27 16:29:56 +0800

    Renamed session_info

--- lib/omniauth/strategies/mindvalley.rb
+++ lib/omniauth/strategies/mindvalley.rb
@@ -15,7 +15,8 @@ module OmniAuth
           :id => user_info["response"]["id"],
           :email => user_info["response"]["email"],
           :first_name => user_info["response"]['first_name'],
-          :last_name => user_info["response"]['last_name']
+          :last_name => user_info["response"]['last_name'],
+          :sso_session_id => session_info["response"]["id"]
           # and anything else you want to return to your API consumers
         }
       end
@@ -25,7 +26,8 @@ module OmniAuth
       end
 
       def session_info
-        access_token.post('/sso/session', {:id => uid })
+        params = { ip: request.ip, agent: request.user_agent }
+        @session_info ||= access_token.post('/sso/sessions', params: params).parsed
       end
     end
   end
