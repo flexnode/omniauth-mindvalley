d7f05a2d2685ec1ce0c8f186882d621ff9320607
Edham Arief / edthix <edthix@gmail.com>
2015-04-24 11:52:05 +0800

    Updated using halo's sso client strategy

--- lib/omniauth/strategies/mindvalley.rb
+++ lib/omniauth/strategies/mindvalley.rb
@@ -4,9 +4,42 @@ module OmniAuth
   module Strategies
     class Mindvalley < OmniAuth::Strategies::OAuth2
       option :client_options, {
-        :site => "http://localhost:3002",
-        :authorize_path => "/oauth/authorize"
-      }
+               :site => "http://localhost:3002",
+               :authorize_path => "/oauth/authorize"
+             }
+
+
+      def self.endpoint
+        if ENV['OMNIAUTH_SSO_ENDPOINT'].to_s != ''
+          ENV['OMNIAUTH_SSO_ENDPOINT'].to_s
+        elsif development_environment?
+          ENV['OMNIAUTH_SSO_ENDPOINT'] || 'http://localhost:3000'
+        elsif test_environment?
+          'https://sso.example.com'
+        else
+          fail 'You must set OMNIAUTH_SSO_ENDPOINT to point to the SSO OAuth server'
+        end
+      end
+
+
+      def self.development_environment?
+        defined?(Rails) && Rails.env.development?
+      end
+
+      def self.test_environment?
+        defined?(Rails) && Rails.env.test? || ENV['RACK_ENV'] == 'test'
+      end
+
+      def self.passports_path
+        if ENV['OMNIAUTH_SSO_PASSPORTS_PATH'].to_s != ''
+          ENV['OMNIAUTH_SSO_PASSPORTS_PATH'].to_s
+        else
+          # We know this namespace is not occupied because /oauth is owned by Doorkeeper
+          '/oauth/sso/v1/passports'
+        end
+      end
+
+
 
       uid { user_info["response"]["id"] }
 
